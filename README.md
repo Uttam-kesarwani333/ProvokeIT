acc42f6ab9bc296610db59c1574505b5092a92ae
Uttam Kesarwani <98887675+Uttam-kesarwani333@users.noreply.github.com>
2023-11-17 12:41:07 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+PROVOKE IT
+ASSIGNMENT TEST (17 NOV 23)
+![1](https://github.com/PradumnSrivastava/Provoke-IT/assets/131151467/c64cc902-56a0-4f7a-9698-9d48149728cd)
+![2](https://github.com/PradumnSrivastava/Provoke-IT/assets/131151467/e7a26e99-3964-44d1-915d-d6a7eda1cddb)
+![3](https://github.com/PradumnSrivastava/Provoke-IT/assets/131151467/c1045f90-98af-4794-9339-48b3f6194d8e)
+![4](https://github.com/PradumnSrivastava/Provoke-IT/assets/131151467/52f4a49e-5d93-4bb7-b3e7-5384b53321fe)
+
+
---
+++ script.js
@@ -0,0 +1,20 @@
+function login() {
+    // Implement login logic here (check credentials, etc.)
+    alert('Login functionality would be implemented in a real application.');
+  }
+  
+  function createAccount() {
+    // Implement account creation logic here (store user info, etc.)
+    alert('Account creation functionality would be implemented in a real application.');
+  }
+  
+  function showLoginForm() {
+    document.getElementById('login-form').style.display = 'block';
+    document.getElementById('create-account-form').style.display = 'none';
+  }
+  
+  function showCreateAccountForm() {
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('create-account-form').style.display = 'block';
+  }
+  
